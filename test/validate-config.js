935b27d95ee9c2b7d431f2ac2a4894b6cfba079a
Nicolas Talle <dev@nicolab.net>
2018-04-06 12:26:59 +0200

    [test] tests different formats of comment

--- test/validate-config.js
+++ test/validate-config.js
@@ -5,7 +5,13 @@ var test = require('tape');
 
 test('load config in eslint to validate all rule syntax is correct', function (t) {
   var CLIEngine = eslint.CLIEngine;
-  var code = 'var foo = 1;\nvar bar = function() {};\nbar(foo);\n';
+  var code = '// comment\n'
+    + '/**************/\n'
+    + '/*-------------------*/\n'
+    + '/*! comment */\n'
+    + '//------------//\n'
+    + '//++++++++++++//\n'
+    + 'var foo = 1;\nvar bar = function() {};\nbar(foo);\n';
 
   var cli = new CLIEngine({
     useEslintrc: false,
